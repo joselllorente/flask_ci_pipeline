bf761d195786f10047706cf1ef45b56d0b845da2
joselllorente <joseluis.llorenteperales@gmail.com>
2025-02-08 09:10:56 +0000

    Flask CI Pipeline

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "¡Hola, Azure DevOps!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
